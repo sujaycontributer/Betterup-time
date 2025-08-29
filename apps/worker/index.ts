2702150aa60cff21f9827a7f4f16f19ee26c401d
Sujay Ghosh <sujayg222@gmail.com>
2025-08-29 20:41:25 +0530

    Worker works fine

--- apps/worker/index.ts
+++ apps/worker/index.ts
@@ -1,12 +1,17 @@
-import {xAck} from "redis_stream/client"
+import { xAck, xReadGroup } from "redis_stream/client"
 
-const REGION_ID = process.env.REGION_ID;
-const WORKER_ID = process.env.WORKER_ID;
+const REGION_ID = process.env.REGION_ID!;
+const WORKER_ID = process.env.WORKER_ID!;
+
+if(!REGION_ID || !WORKER_ID) throw new Error("REGION_ID OR WORKER_ID is not there");
 
 async function main() {
 
-    while(1) {
-        
-    }
+    
+        const res: any = await xReadGroup(REGION_ID, WORKER_ID);
+        console.log(res[0].messages);
+    // }
 }
+
+main();
 
--- packages/redis_stream/index.ts
+++ packages/redis_stream/index.ts
@@ -5,20 +5,35 @@ const client = await createClient()
   .connect();
 
 type websiteType = {id: string, url: string};
+const STREAM_NAME = 'betteruptime:website'
 
 async function xAdd({url, id}: websiteType) {
     const res = await client.xAdd(
-        'betteruptime:website', '*', {
+        STREAM_NAME, '*', {
            id,
            url
         }
     );
-
 }
 
 export async function xAddBulk(websites: websiteType[]) {
-    
+    console.log(websites.length);
     for(let i = 0; i < websites.length; i++ ) {
         await xAdd({url: websites[i]?.url!, id: websites[i]?.id!})
     }
+}
+
+export async function xReadGroup(consumerGroup: string, workerId: string): Promise<any> {
+    const res = await client.xReadGroup(consumerGroup, workerId, {
+        key: STREAM_NAME,
+        id: '>'
+    }, {
+        COUNT: 5
+    });
+
+    return res;
+} 
+
+export async function xAck(consumerGroup: string, streamId: string) {
+    await client.xAck(STREAM_NAME, consumerGroup, streamId);
 }
