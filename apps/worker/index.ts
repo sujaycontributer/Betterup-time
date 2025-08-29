86affbc98869ec92898c14d26abcf8bbf8376e16
Sujay Ghosh <sujayg222@gmail.com>
2025-08-29 19:54:06 +0530

    redis dependency added to worker

---
+++ apps/worker/index.ts
@@ -0,0 +1,12 @@
+import {xAck} from "redis_stream/client"
+
+const REGION_ID = process.env.REGION_ID;
+const WORKER_ID = process.env.WORKER_ID;
+
+async function main() {
+
+    while(1) {
+        
+    }
+}
+
