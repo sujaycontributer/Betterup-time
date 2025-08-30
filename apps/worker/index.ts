f2c6c892e1933c193840224dd08839feb58b666c
Sujay Ghosh <sujayg222@gmail.com>
2025-08-30 22:19:57 +0530

    clean code written for website tick

--- apps/worker/index.ts
+++ apps/worker/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { resolve } from "bun";
 import { xAck, xReadGroup } from "redis_stream/client"
 import {prismaClient} from "store/client"
 
@@ -10,31 +11,46 @@ if(!REGION_ID || !WORKER_ID) throw new Error("REGION_ID OR WORKER_ID is not ther
 async function main() {
     const res: any = await xReadGroup(REGION_ID, WORKER_ID);
     // console.log(res[0].messages);
-    res.map(({id, messages}:any) => {
-        const url = messages.url;
-        const websiteId = messages.id;
-
-        const startTime = Date.now();
-
-        axios.get(url)
-            .then( async () => {
-              // means website is up 
-              await prismaClient.websiteTick.create({
-                data: {
-                    response_time: Date.now() - startTime,
-                    status: "Up",
-                    timeAdded: Date.now().toString(),
-                    region_id: REGION_ID,
-                    website_id: websiteId
-                }
-              });
-            })
-            .catch((err) => {
-                // means website is down
-            })
-
-    });
+    const promises = res.map(({id, messages}:any) => checkStatus(messages.url, messages.id)); // array of promises
     
+}
+
+async function checkStatus(url: string, websiteId: string) {
+  return new Promise<void>((resolve, reject) => {
+        //  const url = messages.url;
+        //  const websiteId = messages.id;
+
+         const startTime = Date.now();
+
+          axios.get(url)
+              .then( async() => {
+                // means website is up 
+                await prismaClient.websiteTick.create({
+                  data: {
+                      response_time: Date.now() - startTime,
+                      status: "Up",
+                      timeAdded: Date.now().toString(),
+                      region_id: REGION_ID,
+                      website_id: websiteId
+                  }
+                });
+                resolve();
+              })
+              .catch(async (err) => {
+                  // means website is down
+                  await prismaClient.websiteTick.create({
+                    data: {
+                        response_time: Date.now() - startTime,
+                        status: "Up",
+                        timeAdded: Date.now().toString(),
+                        region_id: REGION_ID,
+                        website_id: websiteId
+                    }
+                  });
+                  resolve();
+
+              });
+          });
 }
 
 main();
